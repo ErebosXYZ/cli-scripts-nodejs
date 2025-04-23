a7815702e092968c08281ce36f355d00a0d7cc61
ErebosXYZ <robertmaynou@gmail.com>
2025-04-23 20:29:59 +0200

    done

--- 0-show-os-info.js
+++ 0-show-os-info.js
@@ -1,1 +1,12 @@
 // Lee el README
+const os = require('os');
+
+console.log(`Nombre del equipo: ${os.hostname}`);
+console.log(`Edición/Versión del sistema operativo: ${os.type}`);
+
+
+console.log(`Memoria RAM total: ${(os.totalmem / 1073741824).toFixed(0)} GB`);
+
+
+
+
